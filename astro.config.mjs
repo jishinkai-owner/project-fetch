f43837bb3818294c4941d7bf3eb688a5d4d96237
m.chiba <chibam496@gmail.com>
2024-05-28 05:10:39 +0900

    マークダウンのスタイル付と記法の拡張

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
-import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+import remarkMath from "remark-math";
+import remarkToc from 'remark-toc';
+import { remarkAlert } from "remark-github-blockquote-alert";
+import remarkRehype from 'remark-rehype';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
-export default defineConfig({
+export default {
   integrations: [mdx()],
-  //site: 'https://jishinkai.github.io',
-  //base: '/fc2.github.io'
-});
+  markdown: {
+    remarkPlugins: [
+      [remarkToc, { heading: '目次|もくじ|Contents|TOC' }],
+      remarkMath,
+      remarkAlert,
+      [remarkRehype, { footnoteLabel: "脚注" }],
+    ],
+    rehypePlugins: [
+      rehypeKatex,
+    ]
+  }
+}
